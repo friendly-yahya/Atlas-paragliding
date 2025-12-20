866991bf69f941164c95575b4466a25d3e7a3d43
friendly yahya <yahyafarkli63@gmail.com>
2025-12-20 22:21:17 +0100

    Add image tile widget.

--- lib/screens/booking_detail_screen.dart
+++ lib/screens/booking_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:atlas_paragliding/theme/app_theme.dart';
 import 'package:flutter/material.dart';
-
+import 'package:atlas_paragliding/widgets/window_img.dart';
 class BookingDetailScreen extends StatelessWidget {
   const BookingDetailScreen({super.key});
 
@@ -9,38 +9,12 @@ class BookingDetailScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: AppTheme.space16,vertical: AppTheme.space16),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(AppTheme.rounded40),
-          child: GridView.builder(
-            itemCount: 4,
-            padding: EdgeInsets.all(0),
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount:2,
-              mainAxisSpacing: 8, 
-              crossAxisSpacing: 8,
-              childAspectRatio: 1,
-            ),
-            itemBuilder:(context, index) {
-              final images = [
-                'assets/images/glide01.jpg',
-                'assets/images/glide02.jpg',
-                'assets/images/glide03.jpg',
-                'assets/images/hicham.png',
-              ];
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(AppTheme.rounded8),
-                child: Image.asset(
-                  images[index],
-                  fit: BoxFit.cover,
-                  
-                ),
-              );
-            },
-            
-            ),
+        child: Column(
+          children: [
+            Expanded(child: WindowImg()),
+          ],
+        )
         ),
-      ),
     );
   }
   
---
+++ lib/widgets/window_img.dart
@@ -0,0 +1,44 @@
+import 'package:atlas_paragliding/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class WindowImg extends StatelessWidget {
+  const WindowImg({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ClipRRect(
+        borderRadius: BorderRadius.circular(AppTheme.rounded40),
+        child: GridView.builder(
+          itemCount: 4,
+          padding: EdgeInsets.all(0),
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount:2,
+            mainAxisSpacing: 8, 
+            crossAxisSpacing: 8,
+            childAspectRatio: 1,
+          ),
+          itemBuilder:(context, index) {
+            final images = [
+              'assets/images/glide01.jpg',
+              'assets/images/glide02.jpg',
+              'assets/images/glide03.jpg',
+              'assets/images/hicham.png',
+            ];
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(AppTheme.rounded8),
+              child: Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+                
+              ),
+            );
+          },
+          
+          ),
+      ),
+    );
+  }
+  
+}
